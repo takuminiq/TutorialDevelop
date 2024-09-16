be83aee971902df4f8a45062e2344f49c33d4067
takuminiq <chocoppu0930@gmail.com>
2024-09-17 02:37:49 +0900

    削除

--- src/main/java/com/techacademy/service/UserService.java
+++ src/main/java/com/techacademy/service/UserService.java
@@ -1,6 +1,7 @@
 package com.techacademy.service;
 
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,12 +22,23 @@ public class UserService {
         // リポジトリのfindAllメソッドを呼び出す
         return userRepository.findAll();
     }
+
     /** Userを1件検索して返す */
     public User getUser(Integer id) {
         return userRepository.findById(id).get();
     }
+
+    /** Userの登録を行なう */
     @Transactional
     public User saveUser(User user) {
         return userRepository.save(user);
+    }
+
+    /** Userの削除を行なう */
+    @Transactional
+    public void deleteUser(Set<Integer> idck) {
+        for(Integer id : idck) {
+            userRepository.deleteById(id);
+        }
     }
 }
